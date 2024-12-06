3b2be7522b4e41806835b5f6c0acbd0c56b21700
radin <radinizz44@gmail.com>
2024-12-06 12:58:36 +0800

    water nav update

--- app/src/main/java/com/example/practical6/MainActivity.java
+++ app/src/main/java/com/example/practical6/MainActivity.java
@@ -1,19 +1,36 @@
 package com.example.practical6;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+//        bottomNavMenu = findViewById(R.id.bottomNavigationView);
+//
+//        NavController navControl = Navigation.findNavController(this, R.id.NHFMain);
+//        NavigationUI.setupWithNavController(bottomNavMenu, navControl);
+//
+//        navControl.addOnDestinationChangedListener((controller, destination, arguments) -> {
+//            if (destination.getId() == R.id.DestRating || destination.getId() == R.id.DestReport) {
+//                bottomNavMenu.setVisibility(View.GONE);
+//            } else {
+//                bottomNavMenu.setVisibility(View.VISIBLE);
+//            }
+//        });
     }
 }
