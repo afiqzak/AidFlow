7ab031cc2d34f5570219fa573f9206742edf69c7
radin <radinizz44@gmail.com>
2024-12-18 19:27:07 +0800

    update news 4

--- app/src/main/java/com/example/newsmodule/MainActivity.java
+++ app/src/main/java/com/example/newsmodule/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.newsmodule;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +22,19 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavMenu = findViewById(R.id.bottomNavigationView);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.FragmentViewMain);
+        NavController navControl = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(bottomNavMenu, navControl);
+
+        navControl.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.destNewsFull || destination.getId() == R.id.destCam || destination.getId() == R.id.destStoryFull || destination.getId() == R.id.destProjectFull) {
+                bottomNavMenu.setVisibility(View.GONE);
+            } else {
+                bottomNavMenu.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
